4eeb27597e2ce720c7995d2e3b7fc1aa65d6c55d
David García <ccristhiangarcia@gmail.com>
2024-04-02 13:36:07 -0500

    Updating Jetstream Layout

---
+++ resources/views/layouts/partials/header-right-guest.blade.php
@@ -0,0 +1,9 @@
+<div class="flex space-x-5">
+    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500" href="{{ route('login') }}">
+        Login
+    </a>
+    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+        href="{{ route('register') }}">
+        Register
+    </a>
+</div>
